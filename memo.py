4f6328fe6d9dd8cdd844063bdbf5b56a2f79d5d8
brumas2022 <142266554+brumas2022@users.noreply.github.com>
2023-08-30 08:26:59 -0400

    Update memo.py

--- memo.py
+++ memo.py
@@ -5,7 +5,7 @@ st.set_page_config(page_title="Memorando")
 def inserir():
   pass
 
-a1 = st.selectbox("SELECIONE O ASSUNTO DO MEMORANDO", ("AQUISICAO DE MATERIAIS/SERVIÇOS", 'ORDEM DE SERVIÇO", "AUTORIZAÇÃO DE FORNECIMENTO", "ABERTURA DE PROCESSO LICITATORIO"))
+a1 = st.selectbox("SELECIONE O ASSUNTO DO MEMORANDO", ("AQUISICAO DE MATERIAIS/SERVIÇOS", "ORDEM DE SERVIÇO", "AUTORIZAÇÃO DE FORNECIMENTO", "ABERTURA DE PROCESSO LICITATORIO"))
 
 st.write("A opção selecionado foi", a1)
 st.write("O numero do seu memorando é:")

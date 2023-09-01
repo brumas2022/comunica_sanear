24265f4066dd318b9f77307cdf7a3b35e59110d5
brumas2022 <142266554+brumas2022@users.noreply.github.com>
2023-09-01 16:52:08 -0400

    Update memo.py

--- memo.py
+++ memo.py
@@ -4,18 +4,7 @@ import mysql.connector
 st.set_page_config(page_title="Memorando")
 
 def inserir():
-  conexao = mysql.connector.connect(
-        host='localhost',
-        user='root',
-        password='Lula#2022',
-        database='new_schema'
-    )
-    
-      cursor = conexao.cursor()
-      comando = f'SELECT * FROM memorando ORDER BY idmemorando DESC'
-      cursor.execute(comando)
-      resultado = cursor.fetchone()
-      st.write(resultado)
+  pass
   
 st.header("BUSQUE O NUMERO PARA O SEU MEMORANDO")
 
@@ -25,7 +14,7 @@ a1 = st.selectbox("SELECIONE O ASSUNTO DO MEMORANDO", ("AQUISICAO", "ORDEM DE SE
 if a1=="AQUISICAO":
   st.image("https://lh3.googleusercontent.com/pw/AIL4fc8um2_bf-evsXVyabYfkgZ4PJFG9h_yCgJpvjiee9D1WvW10hql_u4AkUT7zrKHyBTiOcS90oSBI1o2wfuQRdVJ1V4iKR4uOC3bbpSCckJ41M_sPe-ZaFZYJoW6FilYuLuMgWsnDRj54cSq2H7A4ShWhBEpYPCDxKm1gVIelZCoTfd865ZHvVcU6l2QZLRWXy2DtEZ2aJQaMJGjnfcRBIitJ8DnESt5WVplS8d3kZ7kmpMdRKPVJL7XT3JV4lI6WvfxsClxApup4gl_W1fwV9ylP1i3oxBHJQyxzfyCD3EE-9gVQwy_tRY9wWOdnjK0TdEqS0uQ5wk2U9vRF45d14X2-3HqRj4dL6uxMq2GpVFguiwbl0UrzmE4CM9S35O8fqndHSOmrouTNx56HdN5JCh208UctJurU7bkoyrDz3fM-As7ZllR1nPnR65CNxA0Kda0Glxas6-vGbrjoW-ZJv29R0fTwGAZtYbfTtmdB6rBZwGkGhb-75nDeMaub1WQ-7jmUGFyoXXL6WMm7YG_b5k8ac5sek8HAYMPRfhC5f0UcC_Odq72qiimeLOu3P-jmD9R5e_rCX_6Ez30vIGKvIyaNfVCtb7yW1NYon76EFy8VQrvZ2cPeVyrslEBCZ1jI6NYETkZSEP2aC6c54ZbqdL2cQHjjTSyZToTpcS-Aj_DRvRfNe6SPpaQg2TaqyybTcStUUO-HQkEwNs3VK2EQgBvupH9tgph8qGjoGXlDacIhbEf-UVvddvE7fZXJTFGXAFHvwoSMLssdVKw3J0D4bWLL4dU3ul0dh9qLdvPd3F95cfh5UGCGyUsjGnkNz7TP6I-7AgyDE3_pv1dWsZQabmxgJ06BM1xUbvnHxE-P4h32Wli_gn38XXLDWmxCsfKMuQLLBHaSJyZxhE=w666-h316-s-no?authuser=0")
 elif a1=="ORDEM DE SERVIÇO":
-  inserir()
+  ##inserir()
 
 st.write("A opção selecionado foi", a1)
 st.write("O numero do seu memorando é:")

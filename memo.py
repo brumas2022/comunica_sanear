b62df2c3d35099267f59e0d29cbdbbd0305a0d26
brumas2022 <142266554+brumas2022@users.noreply.github.com>
2023-12-29 14:48:50 -0400

    Update memo.py

--- memo.py
+++ memo.py
@@ -13,6 +13,9 @@ st.header("ACESSE UM NUMERO DE MEMORANDO")
 
 with st.form(key="novo_memorando"):
   a0 = st.text_input("DIGITE SEU NOME COMPLETO")
+  setor = st.radio("Selecione a diretoria :", ("GERAL", "ADMINISTRATIVA", "TECNICA", "MANUTENCAO", "JURIDICO"))
+  if setor =="GERAL":
+      st.write("foi escolhida a GERAL")
 
   a1 = st.selectbox("SELECIONE O ASSUNTO DO MEMORANDO", ("AQUISICAO", "ORDEM DE SERVIÇO", "AUTORIZAÇÃO DE FORNECIMENTO", "ABERTURA DE PROCESSO LICITATORIO"))
 
